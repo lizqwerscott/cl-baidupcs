b3046995d55517defb12a7b5ab125b20d1ad0515
lizqwer scott <m13994059075@163.com>
2020-08-07 19:13:03 +0800

    "the first commit"

---
+++ main.lisp
@@ -0,0 +1,4 @@
+(in-package :cl-baidupcs)
+
+
+(in-package :cl-user)
---
+++ package.lisp
@@ -0,0 +1,5 @@
+(in-package :cl-user)
+
+(defpackage :cl-baidupcs
+  (:use :common-lisp :uiop :cl-json :cl-selenium) 
+  (:export ))
